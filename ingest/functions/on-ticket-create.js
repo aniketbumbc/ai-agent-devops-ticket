957edf20a74f77011cf031ca2f0f2fb4e622f11b
aniketbumbc <aniket.umbc@gmail.com>
2025-07-02 14:28:26 +0530

    created steps

---
+++ ingest/functions/on-ticket-create.js
@@ -0,0 +1,88 @@
+import { inngest } from '../client';
+import Ticket from '../../models/ticket';
+import { NonRetriableError } from 'inngest';
+import { sendMail } from '../../utils/mailer';
+import analyzeTicket from '../../utils/agent';
+import User from '../../models/user.model';
+
+export const onTicketCreated = inngest.createFunction(
+  { id: 'on-ticket-created', retries: 2 },
+  {
+    event: 'ticket/created',
+  },
+
+  async ({ event, step }) => {
+    try {
+      const { ticketId } = event.data;
+
+      const ticket = await step.run('fetch-ticket', async () => {
+        const ticketObject = await Ticket.findById(ticketId);
+
+        if (!ticketObject) {
+          throw new NonRetriableError('Ticket not found');
+        }
+        return ticketObject;
+      });
+
+      const apiResponse = await analyzeTicket(ticket);
+
+      const relatedSkills = await step.run('ai-processing', async () => {
+        let skills = [];
+
+        if (apiResponse) {
+          await Ticket.findByIdAndUpdate(ticket._id, {
+            priority: !['low', 'medium', 'high'].includes(apiResponse.priority)
+              ? 'medium'
+              : apiResponse.priority,
+            helpfulNotes: apiResponse.helpfulNotes,
+            status: 'IN_PROGRESS',
+            relatedSKills: apiResponse?.relatedSkills,
+          });
+
+          skills = apiResponse?.relatedSkills;
+        }
+
+        return skills;
+      });
+
+      const moderator = await step.run('assign-moderator', async () => {
+        let user = await User.findOne({
+          role: 'moderator',
+          skills: {
+            $eleMatch: {
+              $regex: relatedSkills.join('|'),
+            },
+          },
+        });
+
+        if (!user) {
+          user = await User.findOne({
+            role: 'admin',
+          });
+        }
+
+        await Ticket.findByIdAndUpdate(ticket._id, {
+          assignedTo: user?._id || null,
+        });
+
+        return user;
+      });
+
+      await step.run('send-email', async () => {
+        if (moderator) {
+          const finalTicket = await Ticket.findById(ticket._id);
+          await sendMail(
+            moderator.email,
+            'Ticket assigned',
+            `A new ticket is assigned to ${finalTicket.title}`
+          );
+        }
+      });
+
+      return { success: true };
+    } catch (error) {
+      console.error('Error running step', error.message);
+      return { success: false };
+    }
+  }
+);
---
+++ utils/agent.js
@@ -0,0 +1,78 @@
+import { createAgent, gemini } from '@inngest/agent-kit';
+
+const analyzeTicket = async (ticket) => {
+  const supportAgent = createAgent({
+    model: gemini({
+      model: 'gemini-1.5-flash-8b',
+      apiKey: process.env.GEMINI_API_KEY,
+    }),
+
+    name: 'AI Ticket generate assistance',
+    system: `You are a Ticket Generation Assistant. When given an issue description, your job is to:
+
+    Summarize the issue clearly and briefly.
+    
+    Estimate the priority of the issue as one of: Low, Medium, High, or Critical.
+    
+    Provide helpful notes for support staff or developers that may assist in resolving the issue.
+
+    List relevant technical skills required.
+    
+    Important constraints:
+    
+    Always respond in valid JSON format only.
+    
+    Do not include any markup, HTML, comments, or explanations.
+    
+    Do not wrap the output in markdown or code fences.
+    
+    Return only a valid JSON object with the following structure:
+    
+    {
+    "summary": "Brief summary of the issue",
+    "priority": "Low | Medium | High | Critical",
+    "notes": "Helpful context or suggestions for resolving the issue",
+    "relatedSkills": "skills that required to solve the issue"
+    }`,
+  });
+
+  const response = await supportAgent.run(`You are ticket agent. only return a strict JSON object with no extra text, headers, markdown
+  
+    Analyze the following support ticket and provide a JSON object with
+    - summary: A short 1-2 sentence summary of the issue.
+    - priority: "Low | Medium | High | Critical",
+    - helpfulNotes: helpful context or suggestions.
+    - relatedSkills: skills array which assist to solve that issue.
+
+    please find sample output JSON object
+
+    {
+        "summary": "Brief summary of the issue",
+        "priority": "Low | Medium | High | Critical",
+        "notes": "Helpful context or suggestions for resolving the issue",
+        "relatedSkills: ["React", "MangoDB"];
+    }
+
+    ----
+
+    Ticket information:
+
+    - Title: ${ticket.title},
+    - Description: ${ticket.description}
+  `);
+
+  const raw = response.output[0].content;
+
+  try {
+    const cleaned = raw
+      .replace(/^\s*```json\s*([\s\S]*?)\s*```[\s]*$/i, '$1')
+      .trim();
+
+    return JSON.parse(cleaned);
+  } catch (error) {
+    console.error('JSON parsing AI response failed:', error.message);
+    throw error; // or return null
+  }
+};
+
+export default analyzeTicket;
